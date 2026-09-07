efca134e2a2da31d390150dd4ced707eb55f790e
mjamison286 <mjamison286@gmail.com>
2026-09-07 17:09:12 -0500

    added a key callback that allows to exit the program by hitting esc, and allows to (hopefully) save the program using ctrl s

--- src/main.cpp
+++ src/main.cpp
@@ -3,11 +3,44 @@
 #include "log.hpp"
 #include "gui.hpp"
 
+//global vars for working directory and whatnot.
+static std::string workingDirectory;
+
+//modifier keys for keycallback
+static bool ctrlPressed = false;
+
+void keyCallback(GLFWwindow* window, int key, int scancode, int action, int mods)
+{
+    if(key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+    {
+        cleanupGui(&window);
+    }
+
+    #pragma region "Control Modifiers"
+    if((key == GLFW_KEY_LEFT_CONTROL || key == GLFW_KEY_RIGHT_CONTROL) && action == GLFW_PRESS)
+    {
+        ctrlPressed = true;
+    }
+
+    if(key == GLFW_KEY_S && action == GLFW_PRESS && ctrlPressed)
+    {
+        logVerbose("saved file");
+        writeToFile("placeholder", workingDirectory);
+    }
+
+    if(!(key == GLFW_KEY_LEFT_CONTROL || key == GLFW_KEY_RIGHT_CONTROL) && action != GLFW_PRESS)
+    {
+        ctrlPressed = false;
+    }
+}
+
 int main(int argc, char* argv[])
 {
     GLFWwindow* window = nullptr;
 
     initGui(&window);
+
+    glfwSetKeyCallback(window, keyCallback);
 
     while(!glfwWindowShouldClose(window))
     {
