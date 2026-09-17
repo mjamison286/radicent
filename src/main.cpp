cfcee12689e3cb6edaba9128382d78fd090a414c
mjamison286 <mjamison286@gmail.com>
2026-09-17 00:26:13 -0500

    removed unneeded iodata struct

--- src/main.cpp
+++ src/main.cpp
@@ -7,12 +7,6 @@
 //modifier keys for keycallback
 static bool ctrlPressed = false;
 
-struct IOData
-{
-    std::string input;
-    std::string output;
-};
-
 void keyCallback(GLFWwindow* window, int key, int scancode, int action, int mods)
 {
     if(key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
